e1044670528b6b35d5f34224446785516ed92ff3
SunsetFrost <1262457596@qq.com>
2019-08-19 20:54:43 +0800

    feat: add home tab

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <router-view></router-view>
+        <van-tabbar v-model="active">
+            <van-tabbar-item icon="scan">扫描</van-tabbar-item>
+            <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+            <van-tabbar-item icon="user-o">我的</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Tabbar, TabbarItem } from 'vant';
+
+Vue.use(Tabbar).use(TabbarItem);
+
+export default {
+    name: 'home',
+}
+</script>
---
+++ src/views/Scan.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>扫描</div>
+</template>
+
+<script>
+export default {
+    name: 'scan',
+}
+</script>
