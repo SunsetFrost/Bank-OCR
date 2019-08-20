e319ac9fa317acd6458c1dfd9967485380667458
SunsetFrost <1262457596@qq.com>
2019-08-20 17:36:29 +0800

    feat: 设计首页布局样式

---
+++ src/components/BankCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card-container">
+        <van-skeleton title avatar :row="2" />
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Skeleton } from 'vant';
+
+Vue.use(Skeleton);
+
+export default {
+    data() {
+        return {
+            card: null,
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .card-container {
+        height: 120px;
+        margin: 20px;
+        padding: 10px;
+        border-radius: 6px;
+        background: linear-gradient(to right, #616161, #9e9e9e);
+    }
+</style>
--- src/views/Card.vue
+++ src/views/Card.vue
@@ -1,13 +1,66 @@
 <template>
-    <div>卡片页</div>
+    <div>
+        <section class="search">
+            <van-search
+                v-model="searchValue"
+                placeholder="搜索银行卡OCR记录"
+                shape="round"
+                @search="onSearch"
+            >
+                <div slot="action" @click="onSearch">
+                    <van-button icon="scan" type="info" size="small" style="margin: 0 12px;">拍照</van-button>
+                </div>
+            </van-search>
+        </section>
+        <section class="btn-group">
+            <van-button icon="scan" round  type="info" size="large">拍照</van-button>
+            <van-button icon="add-o" round  size="large">上传图片</van-button>
+        </section>
+        <section class="list">
+            <bank-card 
+                v-for="item in cardList" 
+                :key="item"
+            />
+        </section>
+    </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import { Button, Search, List } from 'vant';
+import BankCard from  '../components/BankCard';
+
+Vue.use(Button);
+Vue.use(Search);
+Vue.use(List);
+
 export default {
-    
+    data() {
+        return {
+            searchValue: '', // 银行卡搜索值
+            cardList: [1, 2, 3, 4, 5, 6, 7, 8],
+        }
+    },
+
+    components: {
+        BankCard,
+    },
+
+    methods: {
+        onSearch() {
+            return null;
+        }
+    }
 }
 </script>
 
 <style lang="less" scoped>
-
+    .btn-group {
+        height: 120px;
+        display: flex;
+        display: -webkit-flex;
+        flex-flow: column nowrap;
+        justify-content: space-around;
+        margin: 20px 16px;
+    }
 </style>
