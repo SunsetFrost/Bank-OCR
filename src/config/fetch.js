07148e41c5b9708bbca005bd754749f935972813
SunsetFrost <1262457596@qq.com>
2019-08-26 16:33:31 +0800

    feat: 修改请求方式支持ajax

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -1,11 +1,14 @@
-import { async } from 'q';
+import { async, reject } from 'q';
 import { baseUrl } from './env';
+import { resolve } from 'url';
 
-export default async (url = '', data = {}, type = 'GET', method = 'fetch') => {
+export default async (url = '', data = {}, type = 'GET', method = 'ajax') => {
+  type = type.toUpperCase();
   url = baseUrl + url;
 
+  // 拼接GET字符串
   if (type == 'GET') {
-    let dataStr = ''; // 数据拼接字符串
+    let dataStr = ''; 
     Object.keys(data).forEach((key) => {
       dataStr += `${key}=${data[key]}&`;
     });
@@ -16,29 +19,61 @@ export default async (url = '', data = {}, type = 'GET', method = 'fetch') => {
     }
   }
 
-  const requestConfig = {
-    credentials: 'include',
-    method: type,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    mode: 'cors',
-  };
-
-  if (type == 'POST') {
-    Object.defineProperty(requestConfig, 'body', {
-      value: JSON.stringify(data),
-    });
-  }
-
-
-  console.log(requestConfig);
-  try {
-    const response = await fetch(url, requestConfig);
-    const responseJson = await response.json();
-    return responseJson;
-  } catch (error) {
-    throw new Error(error);
+  if(window.fetch && method == 'fetch') {
+    const requestConfig = {
+      credentials: 'include',
+      method: type,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+    };
+  
+    if (type == 'POST') {
+      Object.defineProperty(requestConfig, 'body', {
+        value: JSON.stringify(data),
+      });
+    }
+  
+    try {
+      const response = await fetch(url, requestConfig);
+      const responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      throw new Error(error);
+    }
+  } else {
+    return new Promise((resolve, reject) => {
+      let requestObj;
+			if (window.XMLHttpRequest) {
+				requestObj = new XMLHttpRequest();
+			} else {
+				requestObj = new ActiveXObject;
+      }
+      
+      let sendData = '';
+			if (type == 'POST') {
+				sendData = JSON.stringify(data);
+      }
+      
+			requestObj.open(type, url, true);
+			requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      requestObj.send(sendData);
+      
+      requestObj.onreadystatechange = () => {
+				if (requestObj.readyState == 4) {
+					if (requestObj.status == 200) {
+						let obj = requestObj.response
+						if (typeof obj !== 'object') {
+							obj = JSON.parse(obj);
+						}
+						resolve(obj)
+					} else {
+						reject(requestObj)
+					}
+				}
+			}
+    })
   }
 };
